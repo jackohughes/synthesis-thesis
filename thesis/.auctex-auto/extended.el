96082acabbea5db1084d3b10636de5e15e3a8582
Jack Hughes <jack@jackohughes.com>
2024-01-07 22:31:46 +0000

    tweak intro first paragraph

--- thesis/.auctex-auto/extended.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "extended"
- (lambda ()
-   (LaTeX-add-environments
-    '("GRANULEfundefnblock" LaTeX-env-args ["argument"] 2)
-    '("GRANULEdefnblock" LaTeX-env-args ["argument"] 2)))
- :latex)
-
--- thesis/.auctex-auto/names.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "names"
- (lambda ()
-   (LaTeX-add-environments
-    '("GRANULEfundefnblock" LaTeX-env-args ["argument"] 2)
-    '("GRANULEdefnblock" LaTeX-env-args ["argument"] 2)))
- :latex)
-
